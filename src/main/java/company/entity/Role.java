6f2bfada8fbe32b80fb31863956c72da1f65528d
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-12-04 10:14:19 -0500

    Initial commit

---
+++ src/main/java/company/entity/Role.java
@@ -0,0 +1,14 @@
+package company.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Role {
+    private Long id;
+    private String description;
+
+}
