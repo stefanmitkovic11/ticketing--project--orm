86566eb83e29d2157a7e92f3bdab826b01d5022e
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-12-17 12:10:02 -0500

    Ticketing-Security Adding Security Part Config

---
+++ src/main/java/company/config/SecurityConfig.java
@@ -0,0 +1,85 @@
+package company.config;
+
+import company.entity.User;
+import company.entity.common.UserPrincipal;
+import company.service.SecurityService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+
+    private final SecurityService securityService;
+    private final AuthSuccessHandler authSuccessHandler;
+
+    public SecurityConfig(SecurityService securityService, AuthSuccessHandler authSuccessHandler) {
+        this.securityService = securityService;
+        this.authSuccessHandler = authSuccessHandler;
+    }
+
+    //    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//
+//        List<UserDetails> userList = new ArrayList<>();
+//
+//        userList.add(new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+//        userList.add(new User("stefan", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
+//        userList.add(new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_EMPLOYEE"))));
+//
+//
+//        return new InMemoryUserDetailsManager(userList);
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeRequests()
+//                .antMatchers(("/user/**")).hasAuthority("ADMIN")
+//                .antMatchers("/project/**").hasAuthority("MANAGER")
+//                .antMatchers("/task/employee/**").hasAuthority("EMPLOYEE")
+//                .antMatchers("/task/**").hasAuthority("EMPLOYEE")
+//                .antMatchers("/task/**").hasAnyRole("MANAGER","ADMIN")
+                .antMatchers(
+                        "/",
+                        "/login",
+                        "/fragments/**",
+                        "/assets/**",
+                        "/images"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+//                .httpBasic()
+                .formLogin()
+                .loginPage("/login")
+                .loginPage("/")
+//                .defaultSuccessUrl("/welcome")
+                .successHandler(authSuccessHandler)
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(120)
+                .key("company")
+                .userDetailsService(securityService)
+                .and().build();
+    }
+
+
+}
